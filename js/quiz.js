57315efc6bf27ce05fec356a3e70ac7611ad93e2
anatjainasp <anantjainasp@gmail.com>
2024-06-07 23:17:51 +0530

    Second Commit

--- js/quiz.js
+++ js/quiz.js
@@ -32,22 +32,48 @@ let quizData = [
 ];
 
 let currentQuestionIndex = 0;
-let userAnswer=[];
+let userAnswer = [];
+let timeLeft = 59;
+let timer;
 
 const questionContainer = document.getElementById("question-container");
 const optionsContainer = document.getElementById("options-container");
 const scoreContainer = document.getElementById("score-container");
 const nextButton = document.getElementById("next-btn");
 const submitButton = document.getElementById("submit-btn");
+const timerDisplay = document.getElementById("timer");
 
-nextButton.addEventListener("click",loadNextQuestion);
+nextButton.addEventListener("click", loadNextQuestion);
+submitButton.addEventListener("click", showQuizResults);
 
 displayQuestion();
+startTimer();
+
+function updateTimer() {
+    if (timeLeft > 0) {
+        const seconds = timeLeft;
+        const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+        timerDisplay.textContent = displaySeconds;
+        timeLeft--;
+    }
+    else {
+        timerDisplay.textContent = `!!!`;
+        showQuizResults();
+    }
+}
+
+function startTimer() {
+    updateTimer();
+    timer = setInterval(updateTimer, 1000)
+}
 
 function loadNextQuestion() {
     if (currentQuestionIndex < quizData.length - 1) {
         currentQuestionIndex++;
         displayQuestion();
+    }
+    else {
+        showQuizResults();
     }
 }
 
@@ -59,7 +85,7 @@ function selectAnswer(answer) {
         `.quiz-option[data-option="${answer}"]`
     );
     selectedOption.classList.add("selected");
-    userAnswer[currentQuestionIndex]=answer;
+    userAnswer[currentQuestionIndex] = answer;
 }
 
 function displayQuestion() {
@@ -91,16 +117,16 @@ function displayQuestion() {
     });
 }
 function evaluateUserAnswer() {
-    let score=0;
-    quizData.forEach((question,index)=>{
-        if(userAnswer[index]=== question.answer){
-            score+=10;
+    let score = 0;
+    quizData.forEach((question, index) => {
+        if (userAnswer[index] === question.answer) {
+            score += 10;
         }
     });
     return score;
 }
 
 function showQuizResults() {
-    const userScore=evaluateUserAnswer();
-    scoreContainer.textContent=`Your Score: ${userScore} out of ${quizData.length*10}`
+    const userScore = evaluateUserAnswer();
+    scoreContainer.textContent = `Your Score: ${userScore} out of ${quizData.length * 10}`
 }
